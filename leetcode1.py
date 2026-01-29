d2e0bcea933d4c4097cfabfbc03dce99d4bea147
Swathi Paila <pailaswathi750@gmail.com>
2026-01-29 21:44:51 +0530

    Add files via upload

---
+++ leetcode1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n=len(nums)
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+        return []
+solution=Solution()
+nums=[2,7,11,15]
+target=9
+result=solution.twoSum(nums,target)
+
+
+        
